757034c0476acc0899abbc0b1d80d0bc5d5f02e3
tinamanga <christinamanga28@gmail.com>
2025-02-18 18:07:56 +0300

    I have added a new file.

---
+++ test.js
@@ -0,0 +1,17 @@
+console.log(3 > 5);
+console.log(3 < 5);
+
+console.log(5 === '5.00');
+
+
+function myName(name){
+
+    var whoIam='My Name is '+ name+' and I am a Software Engineer!'; 
+
+    console.log("Inside myName Function");
+    return whoIam;
+    
+
+}
+
+console.log(myName("Christina"));
