62a3926b4b697767b7a63d55f626320b403da5dd
tinamanga <christinamanga28@gmail.com>
2025-02-18 17:45:03 +0300

    My first Javascript Lessons

---
+++ index.js
@@ -0,0 +1,77 @@
+console.log('Hello world');
+
+//  we use let to declare a variable
+//let name ='Mosh'
+//console.log(name);
+// cannot be reserved
+// should be meaningful
+//cannot start with a number()
+//cannot contain a space or hyphen
+//are case-sensitive
+ //let FirstName ='Mosh'
+ //let LastName ='Hamedani'
+
+ //primitive/ value type 
+ //String,Boolean,null,undefined,Number
+
+ //reference types
+ //Object,Array,Function.
+
+  //Object
+ 
+//   let person ={
+//     name: 'Mosh',
+//     age: 30
+//   }
+//   console.log(person)
+
+// //dot notation is used When eg 
+// person.name ='John';
+// console.log(person.name);
+// //bracket notation is used when  you dont know the nameof the target property eg 
+
+// let selection = 'name'
+// person[selection] ='Mary'  
+// console.log(person.name);
+// //arrays-used when dealing with list of objects
+// let selectedColors = ['red','blue']
+// console.log(selectedColors);
+// selectedColors[2]='green';
+// selectedColors[2]=1;
+// console.log(selectedColors);
+// console.log(selectedColors.length);
+// console.log(selectedColors[0]);//0 is the index number of the item
+// console.log(selectedColors[1]);//1 is the index number of the item
+
+//checking the type of Array
+
+//functions
+//bulding blocks of javascript
+function greet (name) {
+    console.log('Hello' +  name);
+}
+
+greet ('John');
+
+//Syntax rules-rules to follow when writing the code.
+//with javascript we can do Math like;
+//1+1*3=4 Bodmas is used.
+//order of operations /operator precedence-they calculate from left to right.
+
+//weird behavior of math in javascriptlike;integers,floats
+//when it comes to money we dont use floats to avoid inaccuracies .so we conver bthem in to cents then convert back to dollars at the end.
+
+//how to round a number using Math.round()function
+    //Math.round(2.2)=2
+
+    //STRINGS
+    //eg 'Hello'
+    //alert('Hello');
+    //type cohesion eg 'hello'+3='Hello3'
+
+    //escape character is character=1letter/number/symbol eg
+    //a character can be a number,a letter or a symbolor escape character
+    //'some\ntext'
+    //syntax for writing comments
+
+    v
